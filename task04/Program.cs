7c70ce39f9769f0ac37dee89d17349d85ac0266d
Max <pripuzov@bk.ru>
2023-03-11 12:01:31 +0900

    Изменили решение задачи task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -9,13 +9,12 @@ Console.Write("Введите число №2 ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число №3 ");
 int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(numberB>max ) 
+    max = numberB;
+if (numberC > max)
+    max = numberC;
 
-if(numberA>numberB & numberA>numberC) {
-    Console.WriteLine($"Максимальное число {numberA}");
-}
-if(numberB>numberA & numberB>numberC) {
-    Console.WriteLine($"Максимальное число {numberB}");
-}
-if(numberC>numberB & numberC>numberA) {
-    Console.WriteLine($"Максимальное число {numberC}");
-}
+ Console.WriteLine($"Максимальное число {max}");
+
+
