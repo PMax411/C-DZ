5a06d244cc1708861ffc72dd25707ff925e4fa0b
Max <pripuzov@bk.ru>
2023-03-18 15:28:59 +0900

    решили задачу 15

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool GetWeekday(int number)
+{
+    if (number > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+int number = Prompt("Введите день недели => ");
+if (GetWeekday(number))
+{
+    Console.WriteLine($"день {number} является выходным");
+}
+else
+{
+    Console.WriteLine($"день {number} не является выходным");
+}
