fbf96f017b0b4074441c46bbce1cde84dc454da0
Max <pripuzov@bk.ru>
2023-03-19 13:29:16 +0900

    Решили задачу 18

--- task18/Program.cs
+++ task18/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+
+
+Console.WriteLine("Введите номер четверти ");
+
+int quater = Convert.ToInt32(Console.ReadLine());
+string result = Quater(quater);
+Console.WriteLine(result);
+
+string Quater(int num)
+{
+    if (num == 1)
+        return "x>0 && y>0";
+    if (num == 2)
+        return "x<0 && y>0";
+    if (num == 3)
+        return "x<0 && y<0";
+    if (num == 4)
+        return "x>0 && y<0";
+    return "Введено некоректное значение";
+}
