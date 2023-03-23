c2d463507a6f4e74d19233ca5658aaeb0f384db1
Max <pripuzov@bk.ru>
2023-03-23 23:00:56 +0900

    решили nfcr 27

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumDigit(number);
+Console.WriteLine($"Результат => {result}");
+
+int SumDigit (int num) {
+    int sum = 0;
+    while(num>0){
+       
+        sum += num%10;
+         num = num/10;
+    }
+    return sum;
+}
