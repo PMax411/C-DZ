2d92807ed103c34caca4e23f2e45409c97eda3d6
Max <pripuzov@bk.ru>
2023-03-23 21:04:57 +0900

    решили task28

--- task28/Program.cs
+++ task28/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+
+Console.WriteLine($"Введите число" );
+int number = Convert.ToInt32(Console.ReadLine());
+  int result = ProductNumbers (number);
+Console.WriteLine($"сумма равна {result}");
+
+
+int ProductNumbers (int num) {
+    int product = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        product = product*i;
+        
+    }
+      return product;
+                
+    }
