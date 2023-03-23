24766793ad80ef207c5c654b306426e4905ba5ce
Max <pripuzov@bk.ru>
2023-03-23 20:25:55 +0900

    Решили задачу task26

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine($"Введите число" );
+int number = Convert.ToInt32(Console.ReadLine());
+int result = CountNumbers(number);
+Console.WriteLine($"{result}");
+
+int CountNumbers (int num) {
+    int count = 0;
+    while (num>0)
+    {
+        
+   
+    
+        
+        count++;
+        num/=10;
+    }
+      return count;
+                
+    }
