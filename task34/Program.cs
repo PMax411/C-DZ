a532857e4a0a06f7455fdaefe87a76f0ece1c57b
Max <pripuzov@bk.ru>
2023-03-27 23:07:51 +0900

    Решили задачу task34

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34:
+// 1.Задайте массив заполненный случайными положительными трёхзначными числами.
+//2. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreatearrayRnd(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void Printarray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+int[] array = CreatearrayRnd(9, 100, 999);
+Printarray(array);
+Console.WriteLine();
+
+int result = FindDoubleNumber(array);
+Console.WriteLine($"Количество четных чисел в масиве => {result}");
+
+int FindDoubleNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
