9ff03ed12c5c61b6078325cd65b4a3de66491b5c
Max <pripuzov@bk.ru>
2023-04-02 12:44:34 +0900

    Решили task44

--- task44/Program.cs
+++ task44/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] FiboArray(int number)
+{
+int[] array = new int[number];
+array[0] = 0;
+array[1] = 1;
+for (int i = 2; i < number; i++)
+{
+array[i] = array[i-1] + array[i -2];
+}
+return array;
+}
+
+void PrintArray (int[] arr)
+{
+Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+else Console.Write($"{arr[i]}");
+}
+Console.Write("]");
+}
+
+// int[] fiboarray = FiboArray(num);
+PrintArray(FiboArray(num));
+
+
