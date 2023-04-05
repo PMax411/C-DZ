17bef66ba6841fe347df4f2344a685fd2b4acabf
Max <pripuzov@bk.ru>
2023-04-05 20:50:35 +0900

    Решили task49

--- task49/Program.cs
+++ task49/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrix(int rows, int column, int min, int max)
+{
+    int[,] matrix = new int[rows, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Printarray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 8} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void ChengeSqareEl(int[,] matrix){
+    for (int i = 0; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j+=2)
+        {
+            matrix[i, j] = matrix[i, j]*matrix[i, j];
+        }
+    }
+}
+int [,] arr2d = CreateMatrix(3, 4,1 ,100);
+Printarray(arr2d);
+Console.WriteLine(" ");
+ChengeSqareEl(arr2d);
+Printarray(arr2d);
+
+
+
